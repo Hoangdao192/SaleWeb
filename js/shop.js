08b3a4deb817129f3cf7de21293d59f5791ebdec
Dinh Cuong <cuong2k2hanam@gmail.com>
2022-03-23 00:44:32 +0700

    Shop's page almost done

--- js/shop.js
+++ js/shop.js
@@ -1,13 +1,12 @@
-const labelIeam = document.querySelectorAll(".category__right__content_item_text label")
+// Color item
+
+const labelItem = document.querySelectorAll(".category__right__content_item_text label")
 const inputIteam = document.querySelectorAll(".category__right__content_item_text input")
 let index = 0
-let time = 0
 
-labelIeam.forEach(function(label, index) {
-    console.log(index)
+labelItem.forEach(function(label, index) {
     inputIteam[index].addEventListener("click", function() {
         selectColor(index)
-        console.log("Click!")
     })
 })
 
@@ -16,5 +15,54 @@ function selectColor(index) {
     if (doActive != null) {
         doActive.classList.remove("active")
     }
-    labelIeam[index].classList.add("active")
+    labelItem[index].classList.add("active")
+}
+
+// display category__right
+
+const contenCategotyItem = document.querySelectorAll(".category__left__content")
+const headCategotyItem = document.querySelectorAll(".category__left__head")
+let indexCategotyItem = -1
+
+headCategotyItem.forEach(function(label, index) {
+    headCategotyItem[index].addEventListener("click", function() {
+        diplayContent(index)
+        console.log("Click head!")
+        console.log(index)
+    })
+})
+
+function diplayContent(index) {
+    const doActive = document.querySelector(".action")
+    if (doActive != null) {
+        doActive.classList.remove("action")
+    }
+
+    if (indexCategotyItem != index) {
+        contenCategotyItem[index].classList.add("action")
+        indexCategotyItem = index;
+    } else {
+        indexCategotyItem = -1;
+    }
+}
+
+// select__size--------------------------------------------------------------------------------------
+
+const labelSizeItem = document.querySelectorAll(".category__left__content__size label")
+let indexSize = 0
+
+labelSizeItem.forEach(function(label, indexSize) {
+    labelSizeItem[indexSize].addEventListener("click", function() {
+        selectSize(indexSize)
+        console.log("Click size!")
+        console.log(indexSize)
+    })
+})
+
+function selectSize(indexSize) {
+    const doActive = document.querySelector(".select__size")
+    if (doActive != null) {
+        doActive.classList.remove("select__size")
+    }
+    labelSizeItem[indexSize].classList.add("select__size")
 }
