8f33317e257d1bdcebaedc0fd5594659d038c486
Hoangdao192 <nguyendanghoangdao2002@gmail.com>
2022-03-22 21:24:35 +0700

    Bo sung giao dien cho shop.php, bo sung backend cho shop.php

--- shop.php
+++ shop.php
@@ -1,25 +1,39 @@
+<?php
+include_once "php/product_class.php";
+
+$product = new product;
+$product_show = $product->show_product();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700;800;900&display=swap" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/74e2dc450b.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/a914f93d25.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/shop.css">
     <link rel="stylesheet" href="css/font.css">
     <title>SHOP</title>
 </head>
+
 <body>
     <section class="header">
         <div class="logo">
             <a href=""><img src="images/logo.png"></a>
         </div>
         <div class="menu">
-            <li><a href="index.html">Home</a></li>
-            <li><a href="shop.html">Shop</a></li>
+            <li><a href="index.html">Trang chủ</a></li>
+            <li><a href="shop.html">Cửa hàng</a></li>
             <li><a href="#">Blog</a></li>
-            <li><a href="#">Contacts</a></li>
+            <li><a href="#">Liên lạc</a></li>
         </div>
         <div class="other">
             <li>
@@ -36,8 +50,9 @@
     </section>
     <section class="directory">
         <h1>Shop</h1>
-        <p>Home &nbsp;<span></span>&nbsp;  Shop</p>
+        <p>Home &nbsp;<span></span>&nbsp; Shop</p>
     </section>
+    <!----------------------Content------------------------->
     <section class="content">
         <div class="content-left">
             <div class="search-box">
@@ -45,7 +60,7 @@
                 <i class="fa-solid fa-magnifying-glass"></i>
             </div>
             <div class="categories">
-                <h1>CATEGORIES</h1><i class="show-categories fa-solid fa-chevron-down"></i>
+                <h1>DANH MỤC</h1><i class="show-categories fa-solid fa-chevron-down"></i>
                 <div class="categories-sub-menu">
                     <br><a href="">Nam</a><br>
                     <a href="">Nữ</a><br>
@@ -54,14 +69,37 @@
             </div>
         </div>
         <div class="content-right">
-            <div class="product-item">
-                <img src="https://localhost/salewebbtl/img1.jpg">
+            <div class="content-right-head">
+                <p>Sắp xếp: </p>
+                <select name="sort-type" id="sort-type">
+                    <option value="low-to-high">Giá thấp đến cao</option>
+                    <option value="high-to-low">Giá cao đến thấp</option>
+                    <option value="a-to-z">A đến Z</option>
+                    <option value="z-to-a">Z đến A</option>
+                </select>
             </div>
-            <div class="product-item">
-                <img src="images/product/man/img2.jpg">
+            <div class="content-right-content">
+                <?php
+                if ($product_show) {
+                    $i = 0;
+                    while ($result = $product_show->fetch_assoc()) {
+                ?>
+                    <div class="product-item">
+                        <img src="<?php echo $result['hinhanh'] ?>">
+                        <div class="product-color">
+                            <div class="product-color-item"></div>
+                        </div>
+                        <p class="product-name"><?php echo $result['ten_sanpham'] ?></p>
+                        <p class="product-price"><?php echo $result['giatien'] ?><span>đ</span></p>
+                        <button><i class="fa-xl fa-thin fa-plus"></i></button>
+                    </div>
+                <?php
+                    }
+                }
+                ?>
             </div>
-            <div class="product-item">
-                <img src="images/product/man/img3.jpg">
+            <div class="content-right-bottom">
+                <p><span>1</span><span>2</span><span>3</span></p>
             </div>
         </div>
     </section>
@@ -114,4 +152,5 @@
     </footer>
 </body>
 <script src="javascript/shop.js"></script>
+
 </html>
